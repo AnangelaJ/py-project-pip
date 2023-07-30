af66179331707abb995161ed70c31da56f2c38b8
Anangela Jugo <anangela.jugo@enternova.net>
2023-07-30 11:45:00 -0500

    Archivos de configuracion

---
+++ main.py
@@ -0,0 +1,5 @@
+"""
+    Luego de enlazar el proyecto con GIT debemos ir a la página gitignore.io y generar el archivo respectivo:
+        - indicando los 3 principales SO: Windows, macOs, Linux; y el lenguaje a utilizar, en este caso python.
+    Adicional se debe crear el archivo README.md con las instrucciones necesarias para que los desarrolladores puedan instalar adecuadamente el proyecto
+"""
